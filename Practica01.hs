36be45e00fade11fe536d6a9cdb430e8af2252ad
JessGA14 <jesusga@ciencias.unam.mx>
2025-04-24 08:36:32 -0600

    practica1.hs

--- Practica01.hs
+++ Practica01.hs
@@ -1,4 +1,3 @@
-module Tarea where
 
 --Ejercicio 1
 distanciaPuntos :: (Float,Float) -> (Float,Float) -> Float
@@ -6,9 +5,9 @@ distanciaPuntos (x1, y1) (x2, y2) = sqrt ((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 --Ejercicio 2
 valorAbsoluto :: Int -> Int 
-valorAbsoluto x = 
-     if x > 0 then x 
-     else  x * (-1)
+valorAbsoluto x = if x > 0 
+    then x 
+    else  x * (-1)
 
 --Ejercicio 3 
 pendiente :: (Float, Float) -> (Float, Float) -> Float
@@ -36,37 +35,31 @@ esBisiesto x = x `mod` 4 == 0
 
 --Ejercicio 8
 comparador :: Int -> Int -> Int
-comparador x y =
-    if (x == y) then
-        0
-    else if (x < y) then
-        -1
-    else if (x > y) then
-        1
-    else 
-        000
+comparador x y = if (x == y) 
+    then 0
+    else if (x < y) 
+        then -1
+        else if (x > y) 
+            then 1
+            else  000
 
 --Ejercicio 9
 maximo :: Int -> Int -> Int -> Int
-maximo x y z =
-    if (z > y && z > x ) then
-        z
-    else if ( x > y && x > z ) then 
-        x
-    else if (y > x && y > z) then
-        y
-    else if (x == z)then 
-        x
-    else if (y == x) then 
-        y
-    else if (y == z) then 
-        y
-    else 
-        z
+maximo x y z = if (z > y && z > x ) 
+    then z
+    else if ( x > y && x > z ) 
+        then x
+        else if (y > x && y > z) 
+            then y 
+            else if (x == z)
+                then x
+                else if (y == x) 
+                    then y
+                    else if (y == z) 
+                        then y
+                        else z
 --Ejercicio 10
 esDescendente :: Int -> Int -> Int -> Int -> Bool
-esDescendente x y z w = 
-    if (x > y  &&  y > z && z > w) then
-        True 
-    else 
-        False 
+esDescendente x y z w = if (x > y  &&  y > z && z > w) 
+    then True 
+    else False 
